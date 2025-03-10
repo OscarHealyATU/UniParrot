cb0649097d50b44787e5bfe67360bcd769884091
OscarHealyATU <g00424324@atu.ie>
2025-03-10 14:03:04 +0000

    added login and register to navigation

--- components/navigation.php
+++ components/navigation.php
@@ -1,11 +1,15 @@
 <nav class="nav1"><a href="">
             <img src="assets/logo_no_bg.png" alt="">
-            <ul>
+            <ul class="width-3">
                 <a href="index.php"><li>Feed</li></a>
                 <a href="profile.php"><li>Profile</li></a>
                 <a href="siteRules.php"><li>Rules</li></a>
                 <a href="aboutUs.php"><li>About</li></a>
                 <a href="contact.php"><li>Contact</li></a>
+            </ul>
+            <ul class="width-2">
+                <a href="contact.php"><li>Sign in</li></a>
+                <a href="contact.php"><li>Sign up</li></a>
             </ul>
         </a>
 
