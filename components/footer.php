dff8ffa1878cf3e88d68685df0f29b1ec1f9b0d1
OscarHealyATU <g00424324@atu.ie>
2025-03-25 12:46:45 +0000

    added theme functionality

--- components/footer.php
+++ components/footer.php
@@ -2,4 +2,9 @@
     <p>
         footer: <a href="https://www.w3schools.com/css/css3_transitions.asp" target="_blank">from w3schools</a>
     </p>
+    <script>
+                document.getElementById("theme-toggle").addEventListener("click", function () {
+                    document.documentElement.classList.toggle("Atu-theme");
+                });
+            </script>
 </footer>
--- components/navigation.php
+++ components/navigation.php
@@ -1,16 +1,18 @@
-<nav class="nav1"><a href="">
-            <img src="assets/UI/nav.png" alt="">
-            <ul class="width-3">
-                <a href="index.php"><li>Posts</li></a>
-                <a href="profile.php"><li>Profile</li></a>
-                <a href="siteRules.php"><li>Rules</li></a>
-                <a href="aboutUs.php"><li>About</li></a>
-                <a href="contact.php"><li>Contact</li></a>
-            </ul>
-            <ul class="right-nav width-2">
-                <a href="login.php"><li>Sign in</li></a>
-                <a href="register.php"><li>Sign up</li></a>
-            </ul>
-        </a>
+<nav class="nav1">
+    <a href="">
+        <img src="assets/UI/nav.png" alt="">
+    </a>
+    <ul class="width-3">
+        <a href="index.php"><li>Posts</li></a>
+        <a href="profile.php"><li>Profile</li></a>
+        <a href="siteRules.php"><li>Rules</li></a>
+        <a href="aboutUs.php"><li>About</li></a>
+        <a href="contact.php"><li>Contact</li></a>
+    </ul>
+    <ul class="right-nav width-2">
+        <a href="login.php"><li>Sign in</li></a>
+        <a href="register.php"><li>Sign up</li></a>
+    </ul>
+    <button id="theme-toggle">Toggle Theme</button>
 
-    </nav>
+</nav>
