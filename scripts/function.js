11f2d17383cb43c8bab2686091034a19b7291365
OscarHealyATU <g00424324@atu.ie>
2025-03-27 11:26:31 +0000

    improved layout

--- scripts/function.js
+++ scripts/function.js
@@ -1,7 +1,6 @@
-
-
 function expandMakePost(){
     
     let aside = document.getElementById("make-post-container"); 
     aside.classList.toggle("hide");
+    aside.classList.toggle("flex-column");
 }
